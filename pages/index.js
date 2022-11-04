0627aec729c7eb920f1ac6abcec66b85b7adcf2e
Jon fingas <jon.fingas@gmail.com>
2022-11-04 13:36:40 -0500

    added tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react"
 
-export default function Home() {  
+import 'tailwindcss/tailwind.css'
+
+const btn =
+  "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
+export default function Home() {
   const [Pokemon, SetPokemon] = useState([])
   const MAX_ID = 493
 
@@ -18,14 +23,18 @@ export default function Home() {
   }
 
   return (
-    <div className='pokemon-container'>
-      <h1> Here is your Pokemon </h1>
-      <div><img src={Pokemon.sprites?.front_default} /></div>
-      <div>{Pokemon.name}</div>
-      <div>{Pokemon.weight}</div>
-      <br/>
+    <div className="w-screen h-screen flex flex-col justify-center text-center" >
+      <h1 className="text-3xl font-bold underline"> Here is your Pokemon </h1>
+      <div className="flex flex-col items-center">
+        <img width="256" height="256" layout="fixed" className="animate-fade-in" src={Pokemon.sprites?.front_default} />
+        <div className="text-2xl font-bold capitalize">
+          <div>Name: {Pokemon.name}</div>
+          <div>Weight: {Pokemon.weight}</div>
+        </div>
+      </div>
+      <br />
       <form>
-        <button onSubmit={GetRandomPokemonByID}>Get New Pokemon</button>
+        <button className={btn} onSubmit={GetRandomPokemonByID}>Get New Pokemon</button>
       </form>
     </div>
   )
---
+++ pages/test.js
@@ -0,0 +1,4 @@
+
+export default function Test() {
+    return <div><h1 className="text-xl">Test</h1></div>
+}
